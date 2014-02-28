aec9acd2b9b37bc64501ceac4cac6886292b29fe
Dam Chen <dmchen114@gmail.com>
2014-02-28 17:50:44 +0800

    support print all logs

--- src/dmloggerd.c
+++ src/dmloggerd.c
@@ -1,24 +1,30 @@
 #include "dm_common.h"
 
-int main(int argc, char **argv)
+void readLogs()
 {
     static char s_buffer[4096] = {0};
+    int rlen;
+    while(1){
+        rlen = mmapLogReadLine(s_buffer, 4096);
+        if(rlen > 0){
+            s_buffer[rlen] = 0;
+            printf(s_buffer);
+        }
+        else
+            break;
+    }
+}
+
+int main(int argc, char **argv)
+{
     logInit(NULL);
 
+    readLogs();
     while(1){
         int nWaitRes = mmapLogWaitEvent(-1);
         if(nWaitRes == EVENT_WAIT_NORMAL || nWaitRes == EVENT_WAIT_TIMEOUT)
         {
-            int rlen;
-            while(1){
-                rlen = mmapLogReadLine(s_buffer, 4096);
-                if(rlen > 0){
-                    s_buffer[rlen] = 0;
-                    printf(s_buffer);
-                }
-                else
-                    break;
-            }
+            readLogs();
         }
         else
             break;
